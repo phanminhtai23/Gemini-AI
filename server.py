8e872cc12e8f45191c42cd4b0061d2b9751ecaba
MinhTai <phanminhtai23@gmail.com>
2024-12-19 05:39:44 +0700

    thêm tính năng tự động tạo folder lưu ảnh

--- server.py
+++ server.py
@@ -6,8 +6,11 @@ import google.generativeai as genai
 import generate as text_gen
 import os
 
-IMAGES_PATH = "D:\Project\AI\API-Gemini\images"
-DOCUMENT_PATH = "D:\Project\AI\API-Gemini\documents"
+# Get the current working directory
+current_path = os.getcwd()
+
+IMAGES_PATH = current_path + "/images"
+DOCUMENT_PATH = current_path + "/documents"
 app = Flask(__name__)
 
 @app.get('/')
@@ -35,16 +38,20 @@ def receive_text_and_image():
         input_text = request.form.get('text', '')
         input_image = request.files.get('image')
 
+        # Check if the directory already exists
+        if not os.path.exists(IMAGES_PATH):
+            # Create the directory
+            os.makedirs(IMAGES_PATH)
+            print(f"Directory images created at {IMAGES_PATH}")
+        else:
+            # Save file to the upload directory
+            filepath = os.path.join(IMAGES_PATH, input_image.filename)
+            input_image.save(filepath)
 
-        # Save file to the upload directory
-        filepath = os.path.join(IMAGES_PATH, input_image.filename)
-        input_image.save(filepath)
+            result = text_gen.generate_image(input_text, filepath)
 
-        result = text_gen.generate_image(input_text, filepath)
-
-        # print("result", result)
-
-        return jsonify({'response': result})
+            # print("result", result)
+            return jsonify({'response': result})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
@@ -55,15 +62,23 @@ def receive_text_and_document():
         input_text = request.form.get('text', '')
         input_document = request.files.get('document')
         # print("input_text", input_text, input_document)
-        # Save file to the upload directory
-        filepath = os.path.join(DOCUMENT_PATH, input_document.filename)
-        input_document.save(filepath)
 
-        result = text_gen.generate_document(input_text, filepath)
+        # Check if the directory already exists
+        if not os.path.exists(DOCUMENT_PATH):
+            # Create the directory
+            os.makedirs(DOCUMENT_PATH)
+            print(f"Directory documents created at {DOCUMENT_PATH}")
+        else:
+            # Save file to the upload directory
+            filepath = os.path.join(DOCUMENT_PATH, input_document.filename)
+            input_document.save(filepath)
 
-        # print("result", result)
+            result = text_gen.generate_document(input_text, filepath)
 
-        return jsonify({'response': result})
+            # print("result", result)
+
+            return jsonify({'response': result})
+
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
