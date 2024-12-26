8ef4cfa82ef0025e17e13f7c928d7b94bcf354b1
MinhTai <phanminhtai23@gmail.com>
2024-12-27 00:29:41 +0700

    Fix some bug

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import time
 from generate import reset_globals
 from waitress import serve
 from user_agents import parse
+from datetime import datetime
+import pytz
 
 load_dotenv()
 HOST = os.getenv('HOST')
@@ -20,31 +22,43 @@ DOCUMENT_PATH = current_path + "/documents"
 app = Flask(__name__)
 
 
-# def clearTerminar():
-#     # Kiểm tra hệ điều hành và gọi lệnh clear phù hợp
-#     if os.name == 'nt':  # Windows
-#         os.system('cls')
-#     else:  # Linux or MacOS
-#         os.system('clear')
+def clearTerminar():
+    # Kiểm tra hệ điều hành và gọi lệnh clear phù hợp
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # Linux or MacOS
+        os.system('clear')
+
+
+def get_current_time_hcm():
+    timezone = pytz.timezone('Asia/Ho_Chi_Minh')
+    return datetime.now(timezone).strftime("%d-%m-%Y, %H:%M:%S")
 
 @app.route('/', methods=['GET', 'HEAD'])
 def home():
     if request.method == 'HEAD':
         return jsonify({'response_HEAD': "chào bot nhe!"}), 200  # Trả về phản hồi trống với mã trạng thái 200 cho yêu cầu HEAD
     else:
-        print("________________________________________________________")
-        print("- User accessed the website at:", time.strftime("%d-%m-%Y, %H:%M:%S"))
-        user_agent_string = request.headers.get('User-Agent')
-        user_agent = parse(user_agent_string)
-        platform = user_agent.os.family
-        browser = user_agent.browser.family
-        print("- User agent:", user_agent_string)
-        print("- User platform:", platform)
-        print("- User browser:", browser)
+        try:
+            print("________________________________________________________")
+            print("- User accessed the website at:", get_current_time_hcm())
+            user_agent_string = request.headers.get('User-Agent')
+            user_agent = parse(user_agent_string)
+            platform = user_agent.os.family
+            browser = user_agent.browser.family
+            print("- User agent:", user_agent_string)
+            print("- User platform:", platform)
+            print("- User browser:", browser)
 
-        reset_globals()
-        # clearTerminar()
-        return render_template('index.html', content="Hello World!")
+            reset_globals()
+            # clearTerminar()
+            return render_template('index.html', content="Hello World!")
+        except Exception as e:
+            print("error user_agent", e)
+            
+            reset_globals()
+            # clearTerminar()
+            return render_template('index.html', content="Hello World!")
 
 # Stream the response to the client
 
@@ -121,5 +135,5 @@ def receive_text_and_document():
 
 if __name__ == '__main__':
     print(f"Server is running on http://{HOST}:{PORT}", time.strftime("%d-%m-%Y, %H:%M:%S"))
-    serve(app, host=HOST, port=PORT)
-    # app.run(debug=True, host=HOST, port=PORT)
+    # serve(app, host=HOST, port=PORT)
+    app.run(debug=True, host=HOST, port=PORT)
